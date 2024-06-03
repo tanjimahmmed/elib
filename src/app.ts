dd5a44b71e747fcbea96fa5147a4b41886ad3a3b
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-03 18:32:17 +0600

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import globalErrorHandler from './middlewares/globalErrorHandler';
 
 const app = express();
 
@@ -7,5 +8,9 @@ const app = express();
 app.get('/', (req, res, next) => {
     res.json({message:"Welcom to library api"});
 });
+
+// global error handler
+app.use(globalErrorHandler)
+
 
 export default app;
