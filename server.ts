a99d7881a55d88138ef3f792ba02faa14b7b5ab5
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-03 12:43:06 +0600

    add database setup

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import app from './src/app';
 import { config } from './src/config/config';
+import connectDB from './src/config/db';
 
-const startServer = () => {
+const startServer = async () => {
+    // Connect Database
+    await connectDB();
     const port = config.port || 3000;
     app.listen(port, ()=> {
         console.log(`Listening on port ${port}`);
