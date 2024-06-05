725c91cdd05fcd5627cb244436e8d549537f9942
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-05 11:24:36 +0600

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -48,7 +48,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // Token generation JWT
     const token = sign({sub: newUser._id}, config.jwtSecret as string, {expiresIn: '7d', algorithm: "HS256"});
     // response
-    res.json({accessToken: token});
+    res.status(201).json({accessToken: token});
 
     }catch(err) {
         return next(createHttpError(500, "Error while signin the jwt token"))
@@ -56,4 +56,32 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
    
 }
 
-export {createUser};
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        return next(createHttpError(400, "All fields are required"))
+    }
+
+    // todo: wrap in try catch
+    const user = await userModel.findOne({email});
+    if(!user) {
+        return next(createHttpError(404, "User not found"))
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        return next(createHttpError(400, "Username or password incorrect!"))
+    }
+
+    // Create accesstoken
+    const token = sign({sub: user._id}, config.jwtSecret as string, {
+        expiresIn: "7d",
+        algorithm: "HS256"
+    });
+
+    res.json({accessToken: token})
+}
+
+export {createUser, loginUser};
