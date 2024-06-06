dfafb60c4cfd2e45e8c46f47259c25f07c352985
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-06 17:03:31 +0600

    add cors support

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -55,7 +55,6 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         });
 
         // Delete temp.files
-        // todo: wrap in try catch...
         await fs.promises.unlink(filePath);
         await fs.promises.unlink(bookFilePath);
 
@@ -146,7 +145,6 @@ const updateBook = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const listBooks = async (req: Request, res: Response, next: NextFunction) => {
-    // const sleep = await new Promise((resolve) => setTimeout(resolve, 5000));
 
     try {
         // todo: add pagination.
@@ -192,8 +190,6 @@ const deleteBook = async (req: Request, res: Response, next: NextFunction) => {
     if (book.author.toString() !== _req.userId) {
         return next(createHttpError(403, "You can not update others book."));
     }
-    // book-covers/dkzujeho0txi0yrfqjsm
-    // https://res.cloudinary.com/degzfrkse/image/upload/v1712590372/book-covers/u4bt9x7sv0r0cg5cuynm.png
 
     const coverFileSplits = book.coverImage.split("/");
     const coverImagePublicId =
@@ -205,7 +201,7 @@ const deleteBook = async (req: Request, res: Response, next: NextFunction) => {
     const bookFilePublicId =
         bookFileSplits.at(-2) + "/" + bookFileSplits.at(-1);
     console.log("bookFilePublicId", bookFilePublicId);
-    // todo: add try error block
+
     await cloudinary.uploader.destroy(coverImagePublicId);
     await cloudinary.uploader.destroy(bookFilePublicId, {
         resource_type: "raw",
